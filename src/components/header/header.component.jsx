8e5050e04ea8fcfa78a71a3b72577103b54ca2f1
Cheryl Liao <cheryl@Cheryls-MacBook-Air.local>
2020-02-21 15:56:30 -0300

    cart hidden redux added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-
+import { connect } from 'react-redux';
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -17,8 +18,15 @@ const Header = () => (
       <Link className='option' to='/shop'>
         CONTACT
       </Link>
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-export default Header;
+const mapStateToProps = ({ cart: { hidden } }) => ({
+  
+  hidden
+});
+
+export default connect(mapStateToProps)(Header);
